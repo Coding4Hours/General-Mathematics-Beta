ddad3c4fb5fc0e95406586733bad45d4b055d280
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-07-16 10:48:46 -0700

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: '/service/',
+    prefix: 'https://tomp.app/',
     bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
